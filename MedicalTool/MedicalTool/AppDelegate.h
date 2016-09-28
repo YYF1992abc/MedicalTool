31a0f4866682d995ebb9004f376a840080c49b85
thunder <imthunder@163.com>
2016-09-28 15:22:01 +0800

    first commit

---
+++ MedicalTool/MedicalTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MedicalTool
+//
+//  Created by Thunder on 9/28/16.
+//  Copyright © 2016 healthCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
